cf8fe0cb083c52145fac1700ac0db4c20765d7ba
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2023-06-13 19:33:58 +0600

    Add core

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from question_model import Question
+from quiz_brain import QuizBrain
+
+# Fetch trivia questions from the Open Trivia Database API
+response = requests.get("https://opentdb.com/api.php?amount=50&category=18&type=multiple")
+response.raise_for_status()  # Raise an exception if request fails
+question_data = response.json()["results"]
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    incorrect_answers = question["incorrect_answers"]
+    all_answers = incorrect_answers + [question_answer]
+    new_question = Question(question_text, all_answers, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
---
+++ quiz_brain.py
@@ -0,0 +1,30 @@
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        answer_choices = current_question.choices
+        print(f"Q.{self.question_number}: {current_question.text}")
+        for i, choice in enumerate(answer_choices):
+            print(f"{i+1}. {choice}")
+        user_answer = input("Enter your answer (1-4): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        user_answer = int(user_answer) - 1  # Subtract 1 to get index of answer choice
+        if user_answer == self.question_list[self.question_number-1].choices.index(correct_answer):
+            self.score += 1
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
